ad41905d63665349e514b7250d772cac11c3c83d
Layhout <layhoutworkmail@gmail.com>
2023-11-21 16:55:23 +0700

    u

--- src/js/main.js
+++ src/js/main.js
@@ -11,84 +11,15 @@ const aboutMe = `A web/mobile developer with ${new Date().getFullYear() - 2021}+
 const aboutMeP = document.querySelector("#about_me_text");
 const tiltingFXWrapper = document.querySelectorAll(".tilting-wrapper");
 const tilt_strength = 0.05;
+const allClickCopy = document.querySelectorAll(".click-copy");
+const allSections = document.querySelectorAll("section");
 
-idleBgFX.forEach(d => d.style.animationDelay = `-${Math.round(Math.random() * 10)}s`);
-
-navBtn.addEventListener("click", function () {
-    if (nav.dataset.open === "false") {
-        nav.dataset.open = "true";
-        navLinks.forEach((l, i) => l.style.transitionDelay = `${i * 0.09 + 0.1}s`)
-    } else {
-        nav.dataset.open = "false";
-        navLinks.forEach((l, i) => l.style.transitionDelay = `${i * 0.09}s`)
-    }
-})
-
-themeBtn.addEventListener("click", function () {
-    this.dataset.theme = this.dataset.theme === "light" ? "dark" : "light"
-})
-
-function setNavLinksHoverFX(x, y, width, height) {
-    navLinkContainer.style.setProperty("--ele-pos-x", `${x}px`);
-    navLinkContainer.style.setProperty("--ele-pos-y", `${y}px`);
-    navLinkContainer.style.setProperty("--ele-width", `${width}px`);
-    navLinkContainer.style.setProperty("--ele-height", `${height}px`);
-}
-
-setNavLinksHoverFX(navLinks[0].offsetLeft, navLinks[0].offsetTop, navLinks[0].scrollWidth, navLinks[0].scrollHeight);
-
-navLinks.forEach(l => {
-    l.addEventListener("pointerenter", function () {
-        setNavLinksHoverFX(this.offsetLeft, this.offsetTop, this.scrollWidth, this.scrollHeight);
-        navLinkContainer.style.setProperty("--ele-opacity", 1);
-    })
-
-    l.addEventListener("pointerleave", function () {
-        navLinkContainer.style.setProperty("--ele-opacity", 0);
-    })
-});
-
-document.addEventListener("mousemove", function (e) {
-    const move_x = window.innerWidth / 2 - e.clientX;
-    const move_y = window.innerHeight / 2 - e.clientY;
-    allDFX.forEach(a => {
-        a.style.transform = `translate(${-(move_x * 0.02)}px, ${-(move_y * 0.05)}px)`;
-    })
-    allIDFX.forEach(a => {
-        a.style.transform = `translate(${move_x * 0.02}px, ${move_y * 0.05}px)`;
-    })
-});
-
-aboutMeP.innerHTML = aboutMe.split(" ").map(w => `<span style="opacity: 0.2">${w}</span>`).join(" ");
-
-tiltingFXWrapper.forEach(t => {
-    t.addEventListener("mousemove", function (e) {
-        console.log(e);
-        const selfProps = this.getBoundingClientRect();
-        const mouseX = e.clientX - selfProps.x;
-        const mouseY = e.clientY - selfProps.y;
-        const halfX = this.scrollWidth / 2;
-        const halfY = this.scrollHeight / 2;
-
-        const tiltBody = this.querySelector(".tilting-body");
-        tiltBody.style.transform = ` perspective(800px) rotateY(${(halfX - mouseX) * -tilt_strength}deg) rotateX(${(halfY - mouseY) * tilt_strength}deg)`;
-        tiltBody.style.transitionDuration = "0.1s";
-
-        const topLayer = this.querySelector(".top-layer");
-        topLayer.style.transform = `translateX(${((mouseX / this.scrollWidth) - 0.5) * 4}%) translateY(${((mouseY / this.scrollHeight) - 0.5) * 4}%)`;
-        topLayer.style.transitionDuration = "0.1s";
-    });
-
-    t.addEventListener("mouseleave", function (e) {
-        this.querySelector(".tilting-body").removeAttribute("style");
-        this.querySelector(".top-layer").removeAttribute("style");
-    })
-})
+let onScreenSection = 0;
 
 /**
  * lenis library
  */
-const lenis = new Lenis()
+const lenis = new Lenis();
 
 lenis.on('scroll', (e) => {
     const { animatedScroll } = e;
@@ -113,3 +44,98 @@ requestAnimationFrame(raf);
 /**
  * 
  */
+
+idleBgFX.forEach(d => d.style.animationDelay = `-${Math.round(Math.random() * 10)}s`);
+
+navBtn.addEventListener("click", function () {
+    if (nav.dataset.open === "false") {
+        nav.dataset.open = "true";
+        navLinks.forEach((l, i) => l.style.transitionDelay = `${i * 0.09 + 0.1}s`);
+        navLinks[navLinks.length - 1].addEventListener("transitionend", () => navLinkContainer.style.setProperty("--ele-opacity", 1), { once: true });
+    } else {
+        nav.dataset.open = "false";
+        navLinks.forEach((l, i) => l.style.transitionDelay = `${i * 0.09}s`);
+        navLinkContainer.style.setProperty("--ele-opacity", 0)
+    }
+})
+
+themeBtn.addEventListener("click", function () {
+    this.dataset.theme = this.dataset.theme === "light" ? "dark" : "light"
+})
+
+function setNavLinksHoverFX(x, y, width, height) {
+    navLinkContainer.style.setProperty("--ele-pos-x", `${x}px`);
+    navLinkContainer.style.setProperty("--ele-pos-y", `${y}px`);
+    navLinkContainer.style.setProperty("--ele-width", `${width}px`);
+    navLinkContainer.style.setProperty("--ele-height", `${height}px`);
+}
+
+navLinks.forEach((l, i) => {
+    l.addEventListener("click", function (e) {
+        lenis.scrollTo(`#${this.dataset.section}`, { offset: -100 });
+        onScreenSection = i;
+    })
+});
+
+window.addEventListener("scroll", function (e) {
+    allSections.forEach((as, i) => {
+        const middleScreen = this.scrollY + document.documentElement.clientHeight / 2;
+        if (middleScreen > as.offsetTop && middleScreen < as.offsetTop + as.scrollHeight) {
+            onScreenSection = i;
+            setNavLinksHoverFX(
+                navLinks[onScreenSection].offsetLeft,
+                navLinks[onScreenSection].offsetTop,
+                navLinks[onScreenSection].scrollWidth,
+                navLinks[onScreenSection].scrollHeight
+            );
+        }
+    })
+})
+
+document.addEventListener("mousemove", function (e) {
+    const move_x = window.innerWidth / 2 - e.clientX;
+    const move_y = window.innerHeight / 2 - e.clientY;
+    allDFX.forEach(a => {
+        a.style.transform = `translate(${-(move_x * 0.02)}px, ${-(move_y * 0.05)}px)`;
+    })
+    allIDFX.forEach(a => {
+        a.style.transform = `translate(${move_x * 0.02}px, ${move_y * 0.05}px)`;
+    })
+});
+
+aboutMeP.innerHTML = aboutMe.split(" ").map(w => `<span style="opacity: 0.2">${w}</span>`).join(" ");
+
+tiltingFXWrapper.forEach(t => {
+    t.addEventListener("mousemove", function (e) {
+        const selfProps = this.getBoundingClientRect();
+        const mouseX = e.clientX - selfProps.x;
+        const mouseY = e.clientY - selfProps.y;
+        const halfX = this.scrollWidth / 2;
+        const halfY = this.scrollHeight / 2;
+
+        const tiltBody = this.querySelector(".tilting-body");
+        tiltBody.style.transform = ` perspective(800px) rotateY(${(halfX - mouseX) * -tilt_strength}deg) rotateX(${(halfY - mouseY) * tilt_strength}deg)`;
+        tiltBody.style.transitionDuration = "0.1s";
+
+        const topLayer = this.querySelector(".top-layer");
+        topLayer.style.transform = `translateX(${((mouseX / this.scrollWidth) - 0.5) * 4}%) translateY(${((mouseY / this.scrollHeight) - 0.5) * 4}%)`;
+        topLayer.style.transitionDuration = "0.1s";
+    });
+
+    t.addEventListener("mouseleave", function (e) {
+        this.querySelector(".tilting-body").removeAttribute("style");
+        this.querySelector(".top-layer").removeAttribute("style");
+    })
+})
+
+allClickCopy.forEach(acc => {
+    acc.addEventListener("click", function (e) {
+        const contents = this.querySelector("span");
+        navigator.clipboard.writeText(contents.textContent);
+        this.querySelector("button").innerHTML = "Copied!";
+
+        setTimeout(() => {
+            this.querySelector("button").innerHTML = "Copy";
+        }, 3000)
+    })
+})
